2cca37bf9bdea15bbbfdfa9fbc912efe52719333
AyaNabil <ayanabil297@gmail.com>
2025-03-05 01:55:12 +0200

    Exercise 2.2

--- Beginners/Day-2/Coding Exercise 2.2 BMI Calculator.py
+++ Beginners/Day-2/Coding Exercise 2.2 BMI Calculator.py
@@ -4,5 +4,5 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-BMI = int(weight) / float(height) ** 2
+BMI = float(weight) / float(height) ** 2
 print(int(BMI))
